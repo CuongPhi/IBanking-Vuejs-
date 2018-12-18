3119e82f672dd915bd7c1a102912568cd891b6c3
Cuong weebpal <cuongnp.weebpal@gmail.com>
2018-12-19 00:08:21 +0700

    added theme

--- src/main.js
+++ src/main.js
@@ -1,12 +1,84 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from "vue";
+import VueRouter from "vue-router";
+import App from "./App";
 
-Vue.config.productionTip = false
+// router setup
+import routes from "./routes/routes";
 
+// Plugins
+import GlobalComponents from "./globalComponents";
+import GlobalDirectives from "./globalDirectives";
+import Notifications from "./components/NotificationPlugin";
+
+// MaterialDashboard plugin
+import MaterialDashboard from "./material-dashboard";
+
+import Chartist from "chartist";
+
+// configure router
+const router = new VueRouter({
+  routes, // short for routes: routes
+  linkExactActiveClass: "nav-item active"
+});
+
+Vue.use(VueRouter);
+Vue.use(MaterialDashboard);
+Vue.use(GlobalComponents);
+Vue.use(GlobalDirectives);
+Vue.use(Notifications);
+
+Vue.prototype.$Chartist = Chartist;
+
+/* eslint-disable no-new */
 new Vue({
+  el: "#app",
+  render: h => h(App),
   router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+  data: {
+    Chartist: Chartist
+  }
+  // methods: {
+  //   submit: function () {
+  //     // this.status = "submitting";
+  //     this.$refs.recaptcha.execute();
+  //   },
+  //   onCaptchaVerified: function (recaptchaToken) {
+  //     const self = this;
+  //     self.status = "submitting";
+  //     self.$refs.recaptcha.reset();
+  //     axios.post("https://vue-recaptcha-demo.herokuapp.com/signup", {
+  //       email: self.email,
+  //       password: self.password,
+  //       recaptchaToken: recaptchaToken
+  //     }).then((response) => {
+  //       self.sucessfulServerResponse = response.data.message;
+  //     }).catch((err) => {
+  //       self.serverError = getErrorMessage(err);
+
+
+  //       //helper to get a displayable message to the user
+  //       function getErrorMessage(err) {
+  //         let responseBody;
+  //         responseBody = err.response;
+  //         if (!responseBody) {
+  //           responseBody = err;
+  //         }
+  //         else {
+  //           responseBody = err.response.data || responseBody;
+  //         }
+  //         return responseBody.message || JSON.stringify(responseBody);
+  //       }
+
+  //     }).then(() => {
+  //       self.status = "";
+  //     });
+
+
+  //   },
+  //   onCaptchaExpired: function () {
+  //     this.$refs.recaptcha.reset();
+  //   }
+  // }
+});
---
+++ src/pages/Login.vue
@@ -0,0 +1,68 @@
+<template>
+<div class="content">
+    <div class="md-layout">
+     <div class="md-layout-item md-medium-size-100 md-size-25"> 
+
+     </div>
+
+      <div class="md-layout-item md-medium-size-100 md-size-50">
+          <form>
+      <md-card>
+        <md-card-header data-background-color="green">
+          <h4 class="title">Login</h4>
+        </md-card-header>
+
+        <md-card-content  class="md-layout-item">
+          <div class="md-layout md-layout-item md-small-size-100">
+            <div class="md-layout-item md-small-size-100">
+              <md-field>
+                <label>User Name</label>
+                <md-input v-model="username" type="text"></md-input>
+              </md-field>
+            </div>
+            <div class="md-layout-item md-small-size-100">
+              <md-field>
+                <label>Password</label>
+                <md-input v-model="password" type="password"></md-input>
+              </md-field>
+            </div>
+            <!-- <span>      @verify="onCaptchaVerified"
+                  @expired="onCaptchaExpired"</span> -->
+            <div class="md-layout-item md-size-100 text-right">
+             <div class="input-group">
+                <vue-recaptcha 
+                    ref="recaptcha"                
+                    size="invisible"
+                    sitekey="6LfJA0gUAAAAAA4YDXBubWMll55x5xLpyzu6n60G">
+                    <md-button  :disabled="status==='submitting'" type="submit" class="md-raised md-success">Login</md-button>
+                </vue-recaptcha>
+              </div>
+            </div>
+          </div>
+        </md-card-content>
+      </md-card>
+    </form>
+      </div>
+      <div class="md-layout-item md-medium-size-100 md-size-25">
+        
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  import VueRecaptcha from 'vue-recaptcha';
+
+    export default {
+        components: {
+            "vue-recaptcha": VueRecaptcha 
+        },
+        data(){
+            return {
+                username: null,
+                password: null,
+                status: "submitting"
+            }
+                       
+        }
+    };
+</script>
