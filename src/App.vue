b31a9e0b3e50b36cb99cb290a1c2fbf7ea6a04b6
Cuong weebpal <cuongnp.weebpal@gmail.com>
2018-12-20 00:35:46 +0700

    added google recapcha to login page

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,9 @@
-<template>  
+<template> 
   <router-view></router-view>
 </template>
 <script>
-export default { 
-  data() {
-    return {
-      username: null,
-      password: null,
-      isLogin: false
-    }
-  }
-};
+export default {};
 </script>
 <style>
 </style>
-<script>
-export default {};
-</script>
+
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,68 +1,68 @@
 <template>
-<div class="content">
+  <div class="content">
     <div class="md-layout">
-     <div class="md-layout-item md-medium-size-100 md-size-25"> 
+      <div class="md-layout-item md-medium-size-100 md-size-30"></div>
 
-     </div>
+      <div class="md-layout-item md-medium-size-100 md-size-40">
+        <form>
+          <md-card>
+            <md-card-header data-background-color="green">
+              <h4 class="title">Login</h4>
+            </md-card-header>
 
-      <div class="md-layout-item md-medium-size-100 md-size-50">
-          <form>
-      <md-card>
-        <md-card-header data-background-color="green">
-          <h4 class="title">Login</h4>
-        </md-card-header>
-
-        <md-card-content  class="md-layout-item">
-          <div class="md-layout md-layout-item md-small-size-100">
-            <div class="md-layout-item md-small-size-100">
-              <md-field>
-                <label>User Name</label>
-                <md-input v-model="username" type="text"></md-input>
-              </md-field>
-            </div>
-            <div class="md-layout-item md-small-size-100">
-              <md-field>
-                <label>Password</label>
-                <md-input v-model="password" type="password"></md-input>
-              </md-field>
-            </div>
-            <!-- <span>      @verify="onCaptchaVerified"
-                  @expired="onCaptchaExpired"</span> -->
-            <div class="md-layout-item md-size-100 text-right">
-             <div class="input-group">
-                <vue-recaptcha 
-                    ref="recaptcha"                
-                    size="invisible"
-                    sitekey="6LfJA0gUAAAAAA4YDXBubWMll55x5xLpyzu6n60G">
-                    <md-button  :disabled="status==='submitting'" type="submit" class="md-raised md-success">Login</md-button>
-                </vue-recaptcha>
+            <md-card-content class="md-layout-item">
+              <div class="md-layout">
+                <div class="md-layout-item md-small-size-100 md-size-100ss">
+                  <md-field>
+                    <label>User Name</label>
+                    <md-input v-model="username" type="text"></md-input>
+                  </md-field>
+                </div>
+                <div class="md-layout-item md-small-size-100 md-size-100">
+                  <md-field>
+                    <label>Password</label>
+                    <md-input v-model="password" type="password"></md-input>
+                  </md-field>
+                </div>
+                <div class="md-layout-item md-size-100">
+                  <vue-recaptcha @verify="onVerify" @expired="onExpired" :sitekey="sitekey"></vue-recaptcha>
+                </div>
+                <div class="md-layout-item md-size-100 text-right">
+                  <md-button type="submit" :disabled="status" class="md-raised md-success">Login</md-button>
+                </div>
               </div>
-            </div>
-          </div>
-        </md-card-content>
-      </md-card>
-    </form>
+            </md-card-content>
+          </md-card>
+        </form>
       </div>
-      <div class="md-layout-item md-medium-size-100 md-size-25">
-        
-      </div>
+      <div class="md-layout-item md-medium-size-100 md-size-30"></div>
     </div>
   </div>
 </template>
-<script>
-  import VueRecaptcha from 'vue-recaptcha';
 
-    export default {
-        components: {
-            "vue-recaptcha": VueRecaptcha 
-        },
-        data(){
-            return {
-                username: null,
-                password: null,
-                status: "submitting"
-            }
-                       
-        }
+<script>
+import VueRecaptcha from "vue-recaptcha";
+
+export default {
+  components: {
+    "vue-recaptcha": VueRecaptcha
+  },
+  data() {
+    return {
+      username: null,
+      password: null,
+      sitekey: "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI",
+      status: true
     };
+  },
+  methods: {
+    onSubmit: function() {},
+    onVerify: function(response) {
+      this.status = false;
+    },
+    onExpired: function() {
+      console.log("Expired");
+    }
+  }
+};
 </script>
