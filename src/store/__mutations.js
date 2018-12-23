8e69ff29bad8daaae5611a34fd0e465491ddaba6
Cuong weebpal <cuongnp.weebpal@gmail.com>
2018-12-24 00:05:15 +0700

    added page transaction history + page beneficiary

--- src/store/__mutations.js
+++ src/store/__mutations.js
@@ -23,9 +23,83 @@ export default {
     state.isLogin = false;
     localStorage.current_user = '';
     router.router.push('/login');
+  },
+  GET_TRANS(state, obj) {
+    get_trans(state, obj);
+  },
+  GET_BENEFICIARY(state, obj) {
+    get_beneficiary(state, obj);
   }
 };
+var get_beneficiary = async(state, obj) => {  
+  var obj_ = {...obj }
+  delete obj_.router;
+  try {
+    const auth = await Vue.axios.post( "http://localhost:1704/api/account/beneficiaries/", null , { headers: { "x-access-token": obj_.token }});
+    if(auth) {
+      state.beneficiaries = auth.data;
+      console.log(state.beneficiaries)
+      state.isLogin = true;
+    }
+  } catch (err) {  // access-token expire
+    if (err.response.status === 405) {
+      obj.router.push("/login");
+    }
+  } 
+}
+var get_trans = async (state, obj) => {
+  var obj_ = {...obj }
+  delete obj_.router;
+  try {
+    const auth = await Vue.axios.post( "http://localhost:1704/api/account/transhistory/", null , { headers: { "x-access-token": obj_.token }});
+    if(auth) {
+      state.transactions = auth.data;
+      state.isLogin = true;
+      console.log(state.transactions)
+    }
+  } catch (err) {  // access-token expire
+    if (err.response.status === 405) {
+      // try {      
+      //   const newToken = await Vue.axios.post("http://localhost:1704/api/auth/new_token/", obj_);
+      //   if(newToken) {
+      //       var newUser = JSON.parse(localStorage.current_user);
+      //       var newtoken = newToken.data.access_token;
+      //       newUser.access_token = newtoken;
+      //       obj_.access_token = newtoken;
+      //       localStorage.current_user = JSON.stringify(newUser);
+      //       state.isLogin = true;
+      //       obj.router.push("/table");
 
+      //       // try {
+      //       //   const auth = await Vue.axios.post( "http://localhost:1704/api/account/transhistory/", null , { headers: { "x-access-token": obj_.token }});
+      //       //   if(auth) {
+      //       //       state.transactions = auth.data;
+      //       //       console.log(state.transactions)
+      //       //       obj.router.push("/table");
+
+      //       //   }
+      //       // } catch (err) {
+      //       //   state.isLogin = false;
+      //       //   obj.router.push("/login");
+  
+      //       // }
+      //   }
+      // } catch (err) {
+      //    if (err.response.status === 500) {
+      //       state.isLogin = false;
+      //       obj.router.push("/login");
+
+      //    }
+      // }
+      
+      /**
+       *    access-token expire go to login page
+       */
+      obj.router.push("/login");
+
+    }
+  } 
+}
 var re_login = async (state, obj) => {
   state.isLogin = false;
   var obj_ = {...obj }
